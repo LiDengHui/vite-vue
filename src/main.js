65d64a5145620acfb61e292aa5cff0b30409d220
lidenghui <denghui.li@thoughtworks.com>
2020-07-17 23:46:09 +0800

    修改打包路径

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import './store/index'
+/**
+ * 
+ * TODO
+ * firebase
+ *  import './store/index'
+ */
 
 createApp(App).mount('#app')
